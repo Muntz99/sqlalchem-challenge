a351c99fa36ee72e43988bdfce6b0922cd997b47
Michael Munson <Muntz_99@outlook.com>
2020-09-19 00:28:32 -0700

    final updates on Homework

--- Resources/api.py
+++ Resources/api.py
@@ -67,8 +67,11 @@ def stations():
     return jsonify(stationOneDimension)
 
 @app.route("/api/v1.0/tobs")
-def tobs():
+def temp_monthly():
+    """Temperature Observations for Top Station for Last Year"""
+    
     session = Session(engine)
+    
     #find last date in database from Measurements 
     last_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first().date
 
@@ -78,18 +81,45 @@ def tobs():
 
     #calculate date one year after last date using timedelta datetime function
     first_date = last_date - timedelta(days=365)
-    
-    station_counts = session.query(Measurement.station, func.count(Measurement.station)).\
-    group_by(Measurement.station).order_by(func.count(Measurement.station).desc()).all()
-    
+    # List the stations and the counts in descending order.
+    station_counts = session.query(Measurement.station, func.count(Measurement.station)).group_by(Measurement.station).order_by(func.count(Measurement.station).desc()).all()
+    # Create top station variable from tuple
     top_station = (station_counts[0])
     top_station = (top_station[0])
-    
+    # Using the station id from the previous query, calculate the lowest temperature recorded, 
+    # highest temperature recorded, and average temperature of the most active station?
     session.query(func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs)).\
     filter(Measurement.station == top_station).all()
-    
+    # Choose the station with the highest number of temperature observations.
+    # Query the last 12 months of temperature observation data for this station and plot the results as a histogram
     top_station_year_obs = session.query(Measurement.tobs).\
     filter(Measurement.station == top_station).filter(Measurement.date >= first_date).all()
+    return jsonify(top_station_year_obs)
+
+@app.route("/api/v1.0/temp/<start>")
+@app.route("/api/v1.0/temp/start/end")
+def stats(start=None, end=None):
+    session = Session(engine)
+
+    """Return TMIN, TAVG, TMAX."""
+
+    # Select statement
+    sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
+
+    if not end:
+        # calculate TMIN, TAVG, TMAX for dates greater than start
+        results = session.query(*sel).\
+            filter(Measurement.date >= start).all()
+        # Unravel results into a 1D array and convert to a list
+        temps = list(np.ravel(results))
+        return jsonify(temps)
+
+    # calculate TMIN, TAVG, TMAX with start and stop
+    results = session.query(*sel).\
+        filter(Measurement.date >= start).\
+        filter(Measurement.date <= end).all()
+  
+    return jsonify(results)
 
 
 
